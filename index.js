7af5249358c01f82f5ee0a96ef637d10960f4f19
Erick-ls <esrestrepor@unadvirtual.edu.co>
2025-07-18 16:51:38 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function getTimestap(){
+    return Date.now();
+}
+
+function getLongTime(locale = 'es-ES'){
+
+    const options={
+
+        weekday:'long',
+        year:'numeric',
+        month:'long',
+        day:'numeric',
+        hour:'numeric',
+        minute:'numeric',
+        second:'numeric',
+        timeZoneName:'short'
+    };
+    return  new Date().toLocaleString(locale, options)
+}
+
+module.exports = {
+    getTimestap,
+    getLongTime,
+}
